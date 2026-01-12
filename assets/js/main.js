e093ab6986510f83be5a94fb32aa7ed890aca9e0
hoangnm-ndm <hoangnm.ndm@gmail.com>
2026-01-12 20:19:51 +0700

    pagination

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,56 @@
 const productsElement = document.getElementById("products");
 const url = `http://localhost:3000`;
+const nextBtn = document.getElementsByClassName("next")[0];
+const preBtn = document.getElementsByClassName("pre")[0];
+const currentPage = document.getElementsByClassName("currentPage")[0];
+const nextPage = document.getElementsByClassName("nextPage")[0];
+const prePage = document.getElementsByClassName("prePage")[0];
 
-let content = "";
-axios
-  .get(`${url}/products`)
-  .then((res) => {
-    res.data.forEach((item) => {
-      content += /*html */ `
-        <div>
-          <h2>${item.name}</h2>
-          <p>Gia: ${item.price}</p>
-          <button>Add to cart</button>
-        </div>
-      `;
-    });
-    productsElement.innerHTML = content;
-  })
-  .catch((err) => console.log(err));
+let page = 1;
+let limit = 12;
+
+function fetchProducts() {
+  axios
+    .get(`${url}/products?_start=${limit * (page - 1) + 1}&_limit=${limit}`)
+    .then((res) => {
+      renderProducts(res.data);
+    })
+    .catch((err) => console.log(err));
+}
+
+function renderProducts(data) {
+  let content = "";
+  data.forEach((item) => {
+    content += /*html */ `
+      <div class="product-item">
+      <img src="${item.thumbnail}" alt="" />
+        <h2>${item.title}</h2>
+        <p>Gia: ${item.price}</p>
+        <button>Add to cart</button>
+      </div>
+    `;
+  });
+  productsElement.innerHTML = content;
+}
+
+nextBtn.onclick = function () {
+  page++;
+  fetchProducts();
+  updatePage();
+};
+preBtn.onclick = function () {
+  page--;
+  fetchProducts();
+  updatePage();
+};
+
+function updatePage() {
+  currentPage.innerText = page;
+  nextPage.innerText = page + 1;
+  prePage.innerText = page - 1;
+  // page = 1 && (prePage.style.display = "none");
+}
+
+updatePage();
+
+fetchProducts();
---
+++ phan-tich-nghiep-vu.md
@@ -0,0 +1,36 @@
+# E-commerce Website - Phân tích nghiệp vụ
+
+## Các tính năng cần thiết
+
+- Đăng danh sách sản phẩm.
+- Có các bộ lọc sản phẩm quan trọng (theo danh mục, theo giá, theo đánh giá, sắp xếp, phân trang, tìm kiếm theo tên).
+- Thêm vào giỏ hàng và quản lý giỏ hàng
+- Đăng ký, đăng nhập
+- Quản lý thông tin người dùng
+- Mua hàng và lưu lại lịch sử đơn hàng (Thanh toán bằng hình thức giả lập chuyển khoản)
+
+- Đổi mật khẩu
+- Tính năng yêu thích sản phẩm
+- Tính năng so sánh sản phẩm
+- Tính năng bình luận, đánh giá sản phẩm đã mua.
+
+## Các màn hình cần thiết
+
+- Home page:
+  - Hiển thị danh sách sản phẩm nổi bật (không bao gồm các bộ lọc).
+- Category page:
+  - Hiển thị danh sách sản phẩm theo danh mục với các bộ lọc nâng cao.
+- About us:
+  - Thông tin về cửa hàng.
+- Product details:
+  - Hiển thị chi tiết sản phẩm khi người dùng nhấp vào một sản phẩm.
+  - Tăng giảm số lượng khi thêm sản phẩm vào giỏ hàng.
+- Cart:
+  - CRUD giỏ hàng.
+- Checkout:
+  - Nhập thông tin giao hàng và phương thức thanh toán.
+  - Lưu lại thông tin đơn hàng.
+- User profile:
+  - Hiển thị và cập nhật thông tin người dùng.
+- Đăng ký
+- Đăng nhập
